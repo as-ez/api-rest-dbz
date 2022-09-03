322d0a97a18e55ece724f611989dd94299c71891
astradae <astradaezequiel.tpi@gmail.com>
2022-09-02 21:25:46 -0300

    dto personaje

---
+++ src/main/java/com/alkemy/challenge/dto/PersonajeDTO.java
@@ -0,0 +1,63 @@
+package com.alkemy.challenge.dto;
+
+public class PersonajeDTO {
+
+    private Long id;
+    private String imagen;
+    private String nombre;
+    private Integer peso;
+    private String historia;
+    private String peliculasSeries;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Integer peso) {
+        this.peso = peso;
+    }
+
+    public String getHistoria() {
+        return historia;
+    }
+
+    public void setHistoria(String historia) {
+        this.historia = historia;
+    }
+
+    public String getPeliculasSeries() {
+        return peliculasSeries;
+    }
+
+    public void setPeliculasSeries(String peliculasSeries) {
+        this.peliculasSeries = peliculasSeries;
+    }
+
+    public PersonajeDTO() {
+        super();
+    }
+}
