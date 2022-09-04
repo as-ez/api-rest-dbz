d55fb11b853292dcee6caa5c6ade156b874c1211
astradae <astradaezequiel.tpi@gmail.com>
2022-09-03 22:13:57 -0300

    nombre cambiado

--- src/test/java/com/api_rest/dbz/ApiRestDBZApplicationTests.java
+++ src/test/java/com/api_rest/dbz/ApiRestDBZApplicationTests.java
@@ -1,10 +1,10 @@
-package com.alkemy.challenge;
+package com.api_rest.dbz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AlkemyChallengeApplicationTests {
+class ApiRestDBZApplicationTests {
 
 	@Test
 	void contextLoads() {
